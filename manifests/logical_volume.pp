f89f5ba9fd797103200c068edf5372aa2e1f3bb9
jamc96 <jamc.mejia@gmail.com>
2019-02-28 07:31:54 -0600

    allow user to set the parameter for lvm_device_path pointing to /dev/mapper

--- manifests/logical_volume.pp
+++ manifests/logical_volume.pp
@@ -28,9 +28,8 @@ define lvm::logical_volume (
   $no_sync                           = undef,
   $region_size                       = undef,
   $alloc                             = undef,
+  $lvm_device_path                   = "/dev/${volume_group}/${name}",
 ) {
-
-  $lvm_device_path = "/dev/${volume_group}/${name}"
 
   if $mountpath_require and $fs_type != 'swap' {
     Mount {
